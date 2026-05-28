074757042555fc33ff29a9ab8909f7233aa233f4
Bruno Maia <brunomaia@users.noreply.github.com>
2026-05-28 07:49:06 -0300

    debug: test imports of express and supabase in api/index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,14 @@
+import express from "express";
+import { createClient } from "@supabase/supabase-js";
+import 'dotenv/config';
+
 export default function (req: any, res: any) {
   res.json({
     status: "ok",
-    message: "Minimal Vercel function works!",
-    env: Object.keys(process.env).filter(k => !k.includes("KEY") && !k.includes("SECRET") && !k.includes("PASSWORD"))
+    message: "Imports except Gemini work!"
   });
 }
+
 
 
 
