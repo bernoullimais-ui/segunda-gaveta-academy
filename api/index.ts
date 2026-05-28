4b40924586a410a031dd02f3d43a840be3f9c65d
Bruno Maia <brunomaia@users.noreply.github.com>
2026-05-28 07:51:17 -0300

    debug: test import of GoogleGenAI in api/index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import { createClient } from "@supabase/supabase-js";
 import 'dotenv/config';
+import { GoogleGenAI } from '@google/genai';
 
 export default function (req: any, res: any) {
   res.json({
     status: "ok",
-    message: "Imports except Gemini work!"
+    message: "Imports including Gemini work!"
   });
 }
+
 
 
 
