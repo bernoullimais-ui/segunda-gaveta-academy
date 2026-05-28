a739401f6f348367c48da89c246c2614f75a3fec
Bruno Maia <brunomaia@users.noreply.github.com>
2026-05-28 07:44:34 -0300

    fix: change api/index.ts back to static import of app to force Vercel bundling

--- api/index.ts
+++ api/index.ts
@@ -1,22 +1,4 @@
-let handler: any;
-let startupError: any = null;
+import app from '../app';
+export default app;
 
-try {
-  const appModule = await import('../app');
-  handler = appModule.default;
-} catch (e: any) {
-  startupError = e;
-}
-
-export default function (req: any, res: any) {
-  if (startupError) {
-    res.status(500).json({
-      error: "FUNCTION_STARTUP_ERROR",
-      message: startupError.message || String(startupError),
-      stack: startupError.stack || ""
-    });
-  } else {
-    handler(req, res);
-  }
-}
 
