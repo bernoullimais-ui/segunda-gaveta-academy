d8d47299a157550d9a8fff214943e3b214660052
Bruno Maia <brunomaia@users.noreply.github.com>
2026-05-28 08:01:12 -0300

    fix: import app with .js extension in api/index.ts for ESM compliance

--- api/index.ts
+++ api/index.ts
@@ -1,24 +1,6 @@
-let handler: any;
-let startupError: any = null;
+import app from './_app.js';
+export default app;
 
-try {
-  const appModule = await import('./_app');
-  handler = appModule.default;
-} catch (e: any) {
-  startupError = e;
-}
-
-export default function (req: any, res: any) {
-  if (startupError) {
-    res.status(500).json({
-      error: "FUNCTION_STARTUP_ERROR",
-      message: startupError.message || String(startupError),
-      stack: startupError.stack || ""
-    });
-  } else {
-    handler(req, res);
-  }
-}
 
 
 
