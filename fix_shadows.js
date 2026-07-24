af071ef75534b4bb840edd0e389c3e11d00cfcf5
Bruno Maia <brunomaia@users.noreply.github.com>
2026-07-23 21:41:04 -0300

    chore: remove script

--- fix_shadows.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let file = fs.readFileSync('src/components/PublicCoursePage.tsx', 'utf8');
-file = file.replace(/rgba\(37,99,235/g, 'rgba(var(--primary-rgb)');
-fs.writeFileSync('src/components/PublicCoursePage.tsx', file);
-console.log('Replaced shadows.');
